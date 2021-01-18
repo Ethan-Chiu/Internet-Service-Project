43fcfa28d82dbddf290772ae77879c3a647e0e97
ZOIjimmy <b09902037@ntu.edu.tw>
2021-01-18 08:21:57 +0800

    nothing

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
 	usertype:		{ type: String, required: true },
 	name:				{	type: String, required: true },
 	account:		{ type: String, required: true, unique: true },
+	email:			{ type: String, required: true },
 	password:		{ type: String, required: true },
 	picture:		{ type: String },
 	lastappear:	{ type: { x: Number, y: Number, s: Number }}
