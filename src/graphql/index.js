fc3cc10b85685d8bf9782436bf68918ce46dac23
Ethan-Chiu <ethanchiu0608@gmail.com>
2021-01-19 15:50:04 +0800

    ethan fixed

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,5 +1,4 @@
 export { CREATE_POST_MUTATION } from './ql_main/createPost'
 export { POSTS_QUERY } from './queries'
-export { CREATE_POST_MUTATION } from './mutations'
 export { POSTS_SUBSCRIPTION } from './subscriptions'
 export { DELETE_POST_MUTATION } from './dmutations'
